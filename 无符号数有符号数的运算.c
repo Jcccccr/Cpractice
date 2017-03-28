03f604369bedcb45840402172d91f04192e53b65
JUXINbit <632464297@qq.com>
2017-03-28 22:43:46 +0800

    面试重点

---
+++ 无符号数有符号数的运算.c
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	char a = -1;
+//	signed char b = -1;
+//	unsigned char c = -1;       //无符号数不是不可以输入负数吗 为什么這里输出的是255？
+//	printf("%d %d %d\n", a, b, c);
+//    return 0;
+//}
+
+
+//int main()
+//{
+//	char a = -128;
+//	printf("%u\n", a);    //输出的是什么  
+//    return 0;
+//}
+
+//int main()
+//{
+//	char a = 128;
+//	printf("%u\n", a);
+//    return 0;
+//}
+
+//注意字符类型不止两种  int可以看成signed int  但是char不能默认成signed看待 要根据不同的编译环境
+
+
+
+
+
+
+
+
