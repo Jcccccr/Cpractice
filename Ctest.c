75b493e4dcd4d4e408df2473f19693aadfb62cda
JUXINbit <632464297@qq.com>
2017-07-17 23:59:55 +0800

    C测试

---
+++ Ctest.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#define M 3
+#include<stdio.h>
+
+
+int main()
+{
+	/*int key;
+	
+	scanf("%d", &key);
+	switch (key)
+	{
+	case M:
+
+	default:
+		break;
+	}*/
+	//extern int arr[];   //ÉùÃ÷
+	/*printf("%d\n",sizeof(ColorVal));
+	printf("%d\n", ColorVal);
+	return 0;*/
+
+	int a = 9;
+	int b = 0;
+	int c = 0;
+	printf("%d\n", sizeof(a = b + c));
+}
